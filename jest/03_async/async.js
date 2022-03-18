12a0538920f3f9ff1bd17918e2cee3aa13376b9d
pavelerokhin <pavel.erokhin@rstor.io>
2022-03-18 12:27:39 +0100

    axios mock

--- jest/03_async/async.js
+++ jest/03_async/async.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 class Ajax {
   static echo(data) {
     return new Promise((res, rej) => {
@@ -9,6 +11,17 @@ class Ajax {
         }
       }, 100);
     });
+  }
+
+  static async get() {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      return response.data;
+    } catch (err) {
+      console.error(e);
+    }
   }
 }
 
--- jest/03_async/async.test.js
+++ jest/03_async/async.test.js
@@ -1,4 +1,7 @@
 const Ajax = require("./async");
+const axios = require("axios");
+
+jest.mock("axios");
 
 describe("Ajax: echo", () => {
   test("should return value async", async () => {
@@ -23,6 +26,29 @@ describe("Ajax: echo", () => {
   test("should catch Error in Promise", () => {
     Ajax.echo().catch((err) => {
       expect(err).toBeInstanceOf(Error);
+    });
+  });
+});
+
+describe("Ajax: get", () => {
+  let response;
+  let todos;
+
+  beforeEach(() => {
+    todos = [
+      { id: 1, title: "Todo 1", completed: false },
+      { id: 2, title: "Todo 2", completed: true },
+    ];
+
+    response = {
+      data: { todos },
+    };
+  });
+
+  test("should return data from backend", () => {
+    axios.get.mockReturnValue(response);
+    Ajax.get().then((data) => {
+      expect(data.todos).toEqual(todos);
     });
   });
 });
