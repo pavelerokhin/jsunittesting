1f24028b4995368f131ca303ab22cddf75bbe85d
pavelerokhin <pavel.erokhin@rstor.io>
2022-03-19 16:05:42 +0100

    init testing classes

---
+++ mock_classes/src/SoundPlayerConsumer.js
@@ -0,0 +1,13 @@
+"use strict";
+import SoundPlayer from "./SoundPlayer";
+
+export default class SoundPlayerConsumer {
+  constructor() {
+    this.soundPlayer = new SoundPlayer();
+  }
+
+  playSomethingCool() {
+    const coolSoundFileName = "song.mp3";
+    this.soundPlayer.playSoundFile(coolSoundFileName);
+  }
+}
